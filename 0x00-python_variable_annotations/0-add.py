52d9179e01df7518b872a5062909f0ad62b55742
sam-fsd <kabirusam037@gmail.com>
2024-01-11 19:33:11 +0300

    basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""defines an add function"""
+
+
+def add(a: float, b: float) -> float:
+    """Sums up two floats
+
+    Args:
+        a (float): The first parameter.
+        b (float): The second paramter.
+
+    Return:
+        float: Result of the sum expression
+    """
+    return a + b
