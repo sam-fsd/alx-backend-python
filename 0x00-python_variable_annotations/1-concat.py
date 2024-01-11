a690236793a6daaf7165f2f2bfecce6c8ff9219d
sam-fsd <kabirusam037@gmail.com>
2024-01-11 19:39:41 +0300

    basic annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/use/bin/env python3
+"""defines a function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    :param str1: The first string to be concatenated.
+    :type str1: str
+    :param str2: The second string to be concatenated.
+    :type str2: str
+    :return: The concatenated string.
+    :rtype: str
+    """
+    return str1 + str2
