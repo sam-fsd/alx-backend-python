4b20701d0b45414d6249248d35d2bd58296f3bdd
sam-fsd <kabirusam037@gmail.com>
2024-01-15 22:38:02 +0300

    proper type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: Optional[int] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous function that waits for a random amount of time and returns the delay.
 
