68b59953cf112df37b29b05321e297bfda48543c
sam-fsd <kabirusam037@gmail.com>
2024-01-21 12:17:17 +0300

    [fix:] annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,6 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> list[float]:
     """  Async comprehension coroutine.  """
     return [i async for i in async_generator()]
